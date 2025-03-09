dffaae1c1ac476db90aaa49bd2811d01f9fb94cf
kjsecor <kjsecor@mtu.edu>
2025-03-09 16:08:19 -0400

    changed module-info for media

--- player/src/main/java/module-info.java
+++ player/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module team6 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.media;
 
     opens team6 to javafx.fxml;
     exports team6;
