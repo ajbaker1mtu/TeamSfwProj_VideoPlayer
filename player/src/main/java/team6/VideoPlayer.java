2af1973565114904da2865f2e12ac7000bf7479d
kjsecor <kjsecor@mtu.edu>
2025-03-09 14:50:18 -0400

    Update VideoPlayer.java

--- player/src/main/java/team6/VideoPlayer.java
+++ player/src/main/java/team6/VideoPlayer.java
@@ -7,8 +7,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.FileChooser;
-import java.io.File;
 
 /**
  * JavaFX App
